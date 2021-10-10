3df737bd7fcf9451fc0bf6f43ef530a4b69998e7
HaNaK0 <hampus@hampushuledal.com>
2021-10-10 23:07:22 +0200

    Setup the basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error, path::PathBuf};
+use structopt::StructOpt;
+use serde::{Deserialize, Serialize};
+
+#[derive(StructOpt)]
+enum Opt {
+    Add {
+        #[structopt(parse(from_os_str))]
+        path: PathBuf
+    },
+    List,
+    #[structopt(external_subcommand)]
+    Other(Vec<String>),
+}
+
+#[derive(Serialize, Deserialize)]
+struct Project {
+    name: String,
+    path: PathBuf,
+    project_type: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ProjectType {
+    name: String,
+    commands: Vec<Command>
+}
+
+#[derive(Serialize, Deserialize)]
+struct Command {
+    command: String,
+    arguments: Vec<String>
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let opt = Opt::from_args();
+
+    match opt {
+        Opt::Add { path } => add(path)?,
+        Opt::List => println!("List"),
+        Opt::Other(_) => println!("Other"),
+    }
+
+    Ok(())
+}
+
+fn add(path: PathBuf) -> Result<(), Box<dyn error::Error>> {
+    println!("Path {}", path.display());
+
+    Ok(())
 }
